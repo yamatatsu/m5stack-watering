346d092fe41c2b7b36be82d44d3bb38d428212ae
yamatatsu <the.kinnikuman@gmail.com>
2024-04-09 21:27:38 +0900

    Initial commit

---
+++ test/m5stack-watering.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as M5StackWatering from '../lib/m5stack-watering-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/m5stack-watering-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new M5StackWatering.M5StackWateringStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

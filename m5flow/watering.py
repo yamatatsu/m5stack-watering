99cb6975a555d961a328fdbfa9daf4f2566e0b3c
yamatatsu <the.kinnikuman@gmail.com>
2024-04-10 09:59:14 +0900

    m5stack code

--- m5flow/watering.py
+++ m5flow/watering.py
@@ -1,6 +1,7 @@
 from m5stack import *
 from m5stack_ui import *
 from uiflow import *
+from m5ui import M5ChartGraph, M5BarGraph
 from IoTcloud.AWS import AWS
 from libs.json_py import *
 import time
@@ -17,14 +18,17 @@ Watering_0 = unit.get(unit.WATERING, unit.PORTB)
 
 
 
-label0 = M5Label('label0', x=60, y=55, color=0x000, font=FONT_MONT_14, parent=None)
+bargraph0 = M5BarGraph(20, 141, 280, 30, 0, 500, M5BarGraph.HORIZON, False, 0x0288FB, 0xFFFFFF)
+label0 = M5Label('label0', x=102, y=69, color=0x000, font=FONT_MONT_38, parent=None)
 
 
 Watering_0.set_pump_status(0)
+screen.set_screen_brightness(50)
 aws = AWS(things_name='M5StackWatering001', host='a1j3ua5k30vuoa-ats.iot.ap-northeast-1.amazonaws.com', port=8883, keepalive=60, cert_file_path="/flash/res/watering-cert.pem.crt", private_key_path="/flash/res/watering-private.pem.key")
 aws.start()
 while True:
   label0.set_text(str(Watering_0.get_adc_value()))
+  bargraph0.addSample(2100 - (Watering_0.get_adc_value()))
   aws.publish(str('/M5StackWatering/watering001/moisture'),str((py_2_json({'moisture':(Watering_0.get_adc_value())}))))
   if (Watering_0.get_adc_value()) < 1000:
     Watering_0.set_pump_status(0)

88b05caf9c1ec1cd689795825da8e924e58a46a9
yamatatsu <the.kinnikuman@gmail.com>
2024-04-10 21:36:27 +0900

    iot events simple state machine

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,47 @@
 import {
 	CloudWatchLogsAction,
 	CloudWatchPutMetricAction,
+	IotEventsPutMessageAction,
 } from "@aws-cdk/aws-iot-actions-alpha";
 import * as iot from "@aws-cdk/aws-iot-alpha";
+import {} from "@aws-cdk/aws-iotevents-actions-alpha";
+import * as iotevents from "@aws-cdk/aws-iotevents-alpha";
 import * as cdk from "aws-cdk-lib";
 import * as logs from "aws-cdk-lib/aws-logs";
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, "M5StackWateringStack");
+
+// ======================
+// IoT Events
+
+const input = new iotevents.Input(stack, "Input", {
+	attributeJsonPaths: ["sensorName", "moisture"],
+});
+
+const threshold = iotevents.Expression.fromString("2000");
+const moisture = iotevents.Expression.inputAttribute(input, "moisture");
+
+const wet = new iotevents.State({
+	stateName: "wet",
+	onEnter: [
+		{
+			eventName: "start-detection",
+			condition: iotevents.Expression.currentInput(input),
+		},
+	],
+});
+const dry = new iotevents.State({ stateName: "dry" });
+wet.transitionTo(dry, { when: iotevents.Expression.gte(moisture, threshold) });
+dry.transitionTo(wet, { when: iotevents.Expression.lt(moisture, threshold) });
+
+new iotevents.DetectorModel(stack, "DetectorModel", {
+	detectorKey: "sensorName",
+	initialState: wet,
+});
+
+// ======================
+// IoT Core
 
 const logGroup = new logs.LogGroup(stack, "LogGroup", {
 	logGroupName: "/M5StackWateringRule",
@@ -27,6 +61,7 @@ new iot.TopicRule(stack, "TopicRule", {
 			metricUnit: "None",
 			metricValue: "${moisture}",
 		}),
+		new IotEventsPutMessageAction(input),
 	],
 	errorAction: new CloudWatchLogsAction(logGroup),
 });
